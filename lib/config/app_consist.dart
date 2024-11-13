288f0dacbde3c747cc49a593d94178aaae59c6d8
aminjafari-dev <110681750+aminjafari-dev@users.noreply.github.com>
2024-11-13 19:25:29 +0330

    fix the timer issue and next version (1.0.5)

---
+++ lib/config/app_consist.dart
@@ -0,0 +1,22 @@
+class AppxConsist {
+  static const String iosAppPurchaseKey = "appl_WCLRnMJQwfFNakHOWLtQDwZnjlr";
+  static const String androidAppPurchaseKey = "goog_tLFFEpAjABOMcKVEBltekeaNllT";
+  static const String premiumMainTheme = "basic_theme_life";
+  static const String premiumNoAd = "no_add_pro_feature_life";
+  static const String androidAdKey = "ca-app-pub-7598204727028305/6903144297";
+  static const String iosAdKey = "ca-app-pub-7598204727028305/8754317989";
+
+
+  // static const String androidAdKey = 'ca-app-pub-7598204727028305/7073335262';
+  // static const String iosAdKey = "ca-app-pub-7598204727028305/4378889167";
+}
+
+// ca-app-pub-7598204727028305~1115501114
+
+// ca-app-pub-7598204727028305/6903144297
+
+
+// ios
+// ca-app-pub-7598204727028305~4296328108
+
+// ca-app-pub-7598204727028305/8754317989
---
+++ lib/config/image_path.dart
@@ -0,0 +1,12 @@
+class ImagePath {
+  static const String spotLight = "assets/images/spotlight.png";
+  static const String backing = "assets/images/backing.png";
+  static const String star = "assets/images/star.png";
+  static const String turnDevice = "assets/images/turn_device.png";
+  static const String connectDevice = "assets/images/connect device.png";
+  static const String blueHourglass = "assets/images/blue hourglass.png";
+  static const String redHourglass = "assets/images/red hourglass.png";
+  static const String exit = "assets/images/exit.png";
+  static const String splotlightBlue = "assets/images/splotlight_blue.png";
+  static const String splotlightRed = "assets/images/splotlight_red.png";
+}
