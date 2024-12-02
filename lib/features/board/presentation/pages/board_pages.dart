c111e40b16febe644503d1e4cf8b878cda444188
aminjafari-dev <110681750+aminjafari-dev@users.noreply.github.com>
2024-12-02 12:40:31 +0330

    add entitys

---
+++ lib/features/board/presentation/pages/board_pages.dart
@@ -0,0 +1,34 @@
+import 'package:flutter/material.dart';
+
+class AppxPadding {
+  // ignore: non_constant_identifier_names
+  static get all_0 => const EdgeInsets.all(0);
+  static get all_5 => const EdgeInsets.all(5);
+  static get all_10 => const EdgeInsets.all(10);
+  static get all_19 => const EdgeInsets.all(19);
+  static get all_48 => const EdgeInsets.all(48);
+  static get bottom_10 => const EdgeInsets.only(bottom: 10);
+
+  // ignore: non_constant_identifier_names
+  static get v30_h25 =>
+      const EdgeInsets.symmetric(horizontal: 25, vertical: 30);
+  static get v30_h57 =>
+      const EdgeInsets.symmetric(horizontal: 57, vertical: 30);
+
+  static get v5_h19 => const EdgeInsets.symmetric(horizontal: 19, vertical: 5);
+  static get v9_h20 => const EdgeInsets.symmetric(horizontal: 20, vertical: 9);
+  static get v10_h35 => const EdgeInsets.symmetric(horizontal: 35, vertical: 10);
+  static get v17_h24 => const EdgeInsets.symmetric(horizontal: 24, vertical: 17);
+  static get v17_h19 => const EdgeInsets.symmetric(horizontal: 19, vertical: 17);
+  static get v2_h3 => const EdgeInsets.symmetric(horizontal: 3, vertical: 2);
+
+  //? Horizontal
+  static get h5 => const EdgeInsets.symmetric(horizontal: 5);
+  static get h45 => const EdgeInsets.symmetric(horizontal: 45);
+
+  //? Vertical
+  static get v5 => const EdgeInsets.symmetric(vertical: 5);
+  static get v17 => const EdgeInsets.symmetric(vertical: 17);
+  static get v27 => const EdgeInsets.symmetric(vertical: 27);
+  // static get h45 => const EdgeInsets.symmetric(horizontal: 45);
+}
---
+++ lib/features/exite/domain/entities/domain_entity.dart
@@ -0,0 +1,55 @@
+class AppxStrings {
+  static const String lorem =
+      "Lorem ipsum orem tempus iaculis. Morbi metus elit, sodales at erat vitae, pulvinar hendrerit diam. Curabitur quis congue metus, sed ultricies libero. ";
+  static const String appName = "APP NAME";
+  static const String createGame = "CREATE NEW GAME";
+  static const String connectSpymaster = "CONNECT SPYMASTER";
+  static const String howToPlay = "HOW TO PLAY";
+  static const String next = "Next";
+  static const String done = "Done";
+  static const String copyRight = "Copyright by @AppxLabs";
+  static const String shop = "Shop";
+  static const String premiumVersion = "Premium Version";
+  static const String adFree = "add free";
+  static const String changeTimer = "change timer";
+  static const String themes = "Themes";
+  static const String cadp = "1.99 CAD";
+  static const String cad = "CAD";
+  static const String p = "\$4.99";
+  static const String setting = "Setting";
+  static const String defaultTurnTimer = "Default Turn Timer";
+  static const String defaultTheme = "Default Theme";
+  static const String policies = "Policies";
+  static const String privacy = "Privacy";
+  static const String term = "Term";
+  static const String premiumFeature = "Premium Feature";
+  static const String setTheDefault =
+      "Set the default amount of time for each round";
+  static const String noTimer = "No Timer";
+  static const String secs = "secs";
+  static const String endTurn = "End Turn";
+  static const String selectTheme = "Select Theme";
+  static const String setTimer = "Set Timer";
+  static const String connectSpymasterDeice = "Connect the Spymaster Device";
+  static const String atLeast2DevicesNeeded = "At least 2 devices needed";
+  static const String joinCode = "Join Code";
+  static const String startGame = "Start Game";
+  static const String enterJoinCodeOr =
+      "Enter join code or select scan the QR code to connect spymaster";
+  static const String enterCode = "Enter Code";
+  static const String spymaster = "Spymaster";
+  static const String joinGame = "Join Game";
+  static const String scanQRCode = "Scan QR code";
+  static const String cameraView = "Camer View";
+  static const String qrScanner = "QR Scanner";
+  static const String scanQRCodeTo = "Scan QR code to connect spymaster";
+  static const String areYouSure = "Are you sure you want to exit?";
+  static const String exit = "Exit";
+  static const String continueGame = "Continue Game";
+  static const String blueWins = "Blue Wins!";
+  static const String redWins = "Red Wins!";
+  static const String premiumPlan = "PREMIUM PLAN";
+  static const String unlockTheFollowing = "Unlock the following features with premium";
+  static const String mainTheme = "Main Theme";
+  static const String test = "teest";
+}
